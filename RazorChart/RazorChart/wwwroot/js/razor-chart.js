c8e1f9c1162d13201b87ef2d162b00eaa245cb47
Yanislav Ivanov <frexcho998@gmail.com>
2023-02-26 00:16:01 +0200

    refactoring the labels logic

--- RazorChart/RazorChart/wwwroot/js/razor-chart.js
+++ RazorChart/RazorChart/wwwroot/js/razor-chart.js
@@ -3,15 +3,10 @@ window.insertLabels = () => {
 
     arcs.forEach(x => {
         var pathLen = x.getTotalLength();
-        var labelValueText = x.getAttribute('data-val');
-        var labelCategoryText = x.getAttribute('data-category');
-
         var pathDistance = pathLen * 0.3;
         var midpoint = x.getPointAtLength(pathDistance)
 
         var svg = x.ownerSVGElement;
-        var valueText = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        var categoryText = document.createElementNS("http://www.w3.org/2000/svg", "text");
 
         if (midpoint.x > 100) {
             midpoint.x > 230 ? midpoint.x += 60 : midpoint.x += 20
@@ -24,21 +19,21 @@ window.insertLabels = () => {
         } else {
             midpoint.y < 230 ? midpoint.y -= 60 : midpoint.y -= 20
         }
+        try {
 
-        valueText.setAttribute("x", midpoint.x);
-        valueText.setAttribute("y", midpoint.y + 15);
-        valueText.setAttribute("text-anchor", "middle");
-        valueText.textContent = labelValueText;
-
-        categoryText.setAttribute("x", midpoint.x);
-        categoryText.setAttribute("y", midpoint.y);
-        categoryText.setAttribute("text-anchor", "middle");
-        categoryText.textContent = labelCategoryText;
-
-        svg.appendChild(valueText);
-        svg.appendChild(categoryText);
+            svg.appendChild(createText(midpoint.x, midpoint.y + 15, x.getAttribute('data-val')));
+            svg.appendChild(createText(midpoint.x, midpoint.y, x.getAttribute('data-category')));
+        } catch (err) {
+            console.log()
+        }
     })
+}
 
-
-
+function createText(x, y, childContent) {
+    var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", x);
+    text.setAttribute("y", y);
+    text.setAttribute("text-anchor", "middle");
+    text.textContent = childContent;
+    return text;
 }
